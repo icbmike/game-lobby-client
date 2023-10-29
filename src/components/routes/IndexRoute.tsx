b894a64844f331d22ddd31d52e5f424ad09ffa42
Michael Little <michael.little@xero.com>
2023-10-30 10:22:56 +1300

    Some styling and build out of index route

--- src/components/routes/IndexRoute.tsx
+++ src/components/routes/IndexRoute.tsx
@@ -1,3 +1,39 @@
+import { ChangeEvent, useState } from "react";
+
 export const IndexRoute = () => {
-    return <h1>Index</h1>
+  const [lobbyCode, setLobbyCode] = useState("");
+
+  const onLobbyCodeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLobbyCode(e.target.value.toUpperCase());
+  }
+
+  const onJoinLobbyClick = () => {};
+
+  return (
+    <>
+      <h1 className="margin-bottom-xlarge">Sick ass game lobby</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <a href="/newLobby" className="button margin-bottom">
+          New Lobby
+        </a>
+        <div>
+          <input
+            className="input margin-right"
+            placeholder="Lobby Code"
+            onChange={onLobbyCodeChange}
+            value={lobbyCode}
+          />
+          <button className="button" onClick={onJoinLobbyClick}>
+            Join Lobby
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
