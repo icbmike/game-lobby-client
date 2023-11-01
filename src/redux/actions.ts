e1a8ec3bb303375bd20bdd82d8890f16e4bc3d75
Michael Little <michael.francis.little@gmail.com>
2023-11-01 23:20:32 +1300

    Add done and failed actions for creating a lobby

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,5 +1,8 @@
 import { createAction } from "@reduxjs/toolkit";
+import { Lobby } from "../models";
 
 export const joinLobbyAction = createAction<{lobbyCode: string}>('joinLobby');
 
 export const createLobbyAction = createAction<{lobbySize: number}>('createLobby');
+export const createLobbyDoneAction = createAction<{lobby: Lobby}>('createLobby/DONE');
+export const createLobbyFailedAction = createAction<{error: Error}>('createLobby/Failed');
