367fbffea71da64de9e65cb1e2f30f52f66249ec
Michael Little <michael.francis.little@gmail.com>
2023-11-05 21:36:33 +1300

    Check if not already on lobby url

--- src/redux/effect.ts
+++ src/redux/effect.ts
@@ -5,44 +5,46 @@ import { createMikeEffect } from "./createMikeEffect";
 import { localStorageKeys } from "../constants";
 
 export const createLobbyEffect = createMikeEffect(createLobbyAction, async ({ payload }) => {
-    try {
-        const lobby = await post<Lobby>('/api/lobbies', { lobbySize: payload.lobbySize });
+  try {
+    const lobby = await post<Lobby>('/api/lobbies', { lobbySize: payload.lobbySize });
 
-        return createLobbyDoneAction({ lobby });
-    } catch (error: unknown) {
-        return createLobbyFailedAction({ error: error as Error });
-    }
+    return createLobbyDoneAction({ lobby });
+  } catch (error: unknown) {
+    return createLobbyFailedAction({ error: error as Error });
+  }
 });
 
 export const lobbyCreatedEffect = createMikeEffect(createLobbyDoneAction, async ({ payload }) => {
-    const { code } = payload.lobby;
+  const { code } = payload.lobby;
 
-    window.history.pushState(undefined, '', `/lobby/${code}`);
+  window.history.pushState(undefined, '', `/lobby/${code}`);
 });
 
 export const getLobbyEffect = createMikeEffect(getLobbyAction, async ({ payload }) => {
-    try {
-        const lobby = await get<Lobby>(`/api/lobbies/${payload.lobbyCode}`);
+  try {
+    const lobby = await get<Lobby>(`/api/lobbies/${payload.lobbyCode}`);
 
-        return getLobbyDoneAction({ lobby });
-    } catch (error: unknown) {
-        return getLobbyFailedAction({ error: error as Error });
-    }
+    return getLobbyDoneAction({ lobby });
+  } catch (error: unknown) {
+    return getLobbyFailedAction({ error: error as Error });
+  }
 });
 
 export const joinLobbyEffect = createMikeEffect(joinLobbyAction, async ({ payload }) => {
-    try {
-        const response = await post<JoinLobbyResponse>(`/api/lobbies/${payload.lobbyCode}/players`, { name: payload.name });
+  try {
+    const response = await post<JoinLobbyResponse>(`/api/lobbies/${payload.lobbyCode}/players`, { name: payload.name });
 
-        localStorage.setItem(localStorageKeys.player, JSON.stringify(response.newPlayer));
-        localStorage.setItem(localStorageKeys.lobbyCode, response.lobby.code);
+    localStorage.setItem(localStorageKeys.player, JSON.stringify(response.newPlayer));
+    localStorage.setItem(localStorageKeys.lobbyCode, response.lobby.code);
 
-        return joinLobbyDoneAction(response);
-    } catch (error: unknown) {
-        return joinLobbyFailedAction({ error: error as Error });
-    }
+    return joinLobbyDoneAction(response);
+  } catch (error: unknown) {
+    return joinLobbyFailedAction({ error: error as Error });
+  }
 });
 
 export const restoreSessionEffect = createMikeEffect(restoreSessionAction, async ({ payload }) => {
-  window.history.pushState(undefined, '', `/lobby/${payload.lobbyCode}`);
+  if (window.location.pathname !== `/lobby/${payload.lobbyCode}`) {
+    window.history.pushState(undefined, '', `/lobby/${payload.lobbyCode}`);
+  }
 });
