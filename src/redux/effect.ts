b71a811fa6e5b3dc6b2530ecbdada967ab589f27
Michael Little <michael.francis.little@gmail.com>
2023-11-07 23:16:50 +1300

    whitespace

--- src/redux/effect.ts
+++ src/redux/effect.ts
@@ -63,7 +63,6 @@ export const leaveLobbyEffect = createMikeEffect(leaveLobbyAction, async ({ payl
     await navigate(`/`);
 
     return leaveLobbyDoneAction();
-
   }
 
   return leaveLobbyFailedAction({ error: response.error });
